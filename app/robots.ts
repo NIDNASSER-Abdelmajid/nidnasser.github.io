24969fa1169620ba4ca4f8471fe263a82bdc5d0c
Abdelmajid NIDNASSER <149203108+NIDNASSER-Abdelmajid@users.noreply.github.com>
2025-09-21 02:35:00 +0100

    Update base URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://abdelmajidnidnasser.com'
+  const baseUrl = 'https://nidnasser.me'
 
   return {
     rules: [
